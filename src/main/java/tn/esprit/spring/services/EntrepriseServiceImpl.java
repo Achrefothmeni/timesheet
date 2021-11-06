d710a0aa3f0fa033b2a7d8f31388e1d871f74eef
hidayamcharek <hidaya.mcharek@esprit.tn>
2021-11-06 12:09:30 +0100

    added other loggers to entreprise

--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,7 @@ import tn.esprit.spring.repository.EntrepriseRepository;
 
 @Service
 public class EntrepriseServiceImpl implements IEntrepriseService {
+	private static final Logger LOGGER = LogManager.getLogger(EntrepriseServiceImpl.class);
 
 	@Autowired
     EntrepriseRepository entrepriseRepoistory;
@@ -21,13 +24,31 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	DepartementRepository deptRepoistory;
 	
 	public int ajouterEntreprise(Entreprise entreprise) {
+		try {
+			LOGGER.debug("Trait for dev");
+			LOGGER.info("Added a new entreprise successfully");
 		entrepriseRepoistory.save(entreprise);
-		return entreprise.getId();
+		return entreprise.getId();}
+		catch (Exception e)
+		{
+			LOGGER.error(e.getMessage());
+			return 0;
+		}
+
 	}
 
 	public int ajouterDepartement(Departement dep) {
-		deptRepoistory.save(dep);
-		return dep.getId();
+		try {
+			LOGGER.debug("Trait for dev");
+            LOGGER.info("Added a new departement successfully ");
+			deptRepoistory.save(dep);
+			return dep.getId();
+		}
+		catch (Exception e)
+		{
+			LOGGER.error(e.getMessage());
+			return 0;
+		}
 	}
 	
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
@@ -36,6 +57,7 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
 				//Rappel : la classe qui contient mappedBy represente le bout Slave
 				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
+		        LOGGER.info("affect a departement to entreprise");
 				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 				Departement depManagedEntity = deptRepoistory.findById(depId).get();
 				
@@ -45,6 +67,8 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	}
 	
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
+		try {
+			LOGGER.debug("Returned Depatement list successfully");
 		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 		List<String> depNames = new ArrayList<>();
 		for(Departement dep : entrepriseManagedEntity.getDepartements()){
@@ -52,6 +76,12 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 		}
 		
 		return depNames;
+		}
+		catch (Exception e)
+		{
+			LOGGER.error(e.getMessage());
+			return new ArrayList();
+		}
 	}
 
 	@Transactional
@@ -66,7 +96,8 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 
 
 	public Entreprise getEntrepriseById(int entrepriseId) {
-		return entrepriseRepoistory.findById(entrepriseId).get();	
+
+		return entrepriseRepoistory.findById(entrepriseId).get();
 	}
 
 }
