4438fbea1f623b607986badf16d4675bc0acf4ee
hidayamcharek <hidaya.mcharek@esprit.tn>
2021-11-06 11:04:12 +0100

    changes

--- src/test/java/tn/esprit/spring/EmployeeServiceTest.java
+++ src/test/java/tn/esprit/spring/EmployeeServiceTest.java
@@ -11,12 +11,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
 import tn.esprit.spring.services.IEmployeService;
-
-
 public class EmployeeServiceTest {
-	
-	
 
+    @RunWith(SpringRunner.class)
+    @SpringBootTest
 
+    @Autowired
+    IEmployeService employeService;
+
+    @Test
+    public void testContextLoads() {
+        Employe e = new Employe("Achref", "Othmani", "achref.othmani@esprit.tn", "password", true, Role.INGENIEUR );
+        this.employeService.addOrUpdateEmploye(e);
+        assertThat(e.getNom()).isEqualTo("Achref");
+    }
 	
 }
